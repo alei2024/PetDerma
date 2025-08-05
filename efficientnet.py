e4ae03cec11d40bfb9bac585eb2d241c87bd0f63
k1nsom <10225501463@stu.ecnu.edu.cn>
2025-08-05 19:30:38 +0800

    add EfficentNet and rename Cnn

---
+++ efficientnet.py
@@ -0,0 +1,123 @@
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader
+
+
+def get_data_loaders(data_dir, batch_size, num_workers=4):
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+        'valid': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    image_datasets = {x: datasets.ImageFolder(
+                        os.path.join(data_dir, x),
+                        transform=data_transforms[x])
+                      for x in ['train', 'valid', 'test']}
+    dataloaders = {x: DataLoader(image_datasets[x], batch_size=batch_size,
+                                 shuffle=(x=='train'), num_workers=num_workers)
+                   for x in ['train', 'valid', 'test']}
+    return dataloaders, image_datasets['train'].classes
+
+
+def train_and_eval(args):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    loaders, class_names = get_data_loaders(args.data_dir, args.batch_size, args.num_workers)
+    num_classes = len(class_names)
+    print("Classes:", class_names)
+
+    # Load EfficientNet
+    model = models.efficientnet_b0(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=args.lr)
+
+    best_acc = 0.0
+    for epoch in range(1, args.epochs + 1):
+        model.train()
+        running_loss = 0.0
+        for inputs, labels in loaders['train']:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+        epoch_loss = running_loss / len(loaders['train'].dataset)
+
+        model.eval()
+        val_loss = 0.0
+        correct = 0
+        with torch.no_grad():
+            for inputs, labels in loaders['valid']:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+                preds = outputs.argmax(dim=1)
+                correct += (preds == labels).sum().item()
+        val_loss /= len(loaders['valid'].dataset)
+        val_acc = correct / len(loaders['valid'].dataset)
+
+        print(f"Epoch {epoch}/{args.epochs} - "
+              f"Train Loss: {epoch_loss:.4f} | "
+              f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.4f}")
+
+        if val_acc > best_acc:
+            best_acc = val_acc
+            torch.save(model.state_dict(), args.save_path)
+            print("Saved best model with acc: {:.4f}".format(best_acc))
+
+    print("\nRunning on test set...")
+    model.load_state_dict(torch.load(args.save_path))
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in loaders['test']:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            preds = outputs.argmax(dim=1)
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+    print(f"Test Accuracy: {correct/total:.4f}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PetDerma EfficientNet Training')
+    parser.add_argument('--data_dir', type=str, default='data', help='root data directory')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    parser.add_argument('--num_workers', type=int, default=4, help='dataloader workers')
+    parser.add_argument('--save_path', type=str, default='best_model.pth', help='path to save best model')
+    args = parser.parse_args()
+    train_and_eval(args)
